66e4d2b413983b639c807214d1175ceb900082ee
Jose Maria Abreu <jose.maria.abreu@accenture.com>
2020-11-26 15:55:25 +0800

    PHP Unit test update

--- local/gugcat/lib.php
+++ local/gugcat/lib.php
@@ -26,8 +26,8 @@
 function local_gugcat_extend_navigation_course($parentnode, $course, $context) {
     $url = new moodle_url('/local/gugcat/index.php', array('id' => $course->id));
     $gugcat = get_string('navname', 'local_gugcat');
-    $icon = new pix_icon('my-media', '', 'local_mymedia');
-    $main_node = $parentnode->add($gugcat, $url, navigation_node::TYPE_CONTAINER, $gugcat, 'gugcat', $icon);
+    $icon = new pix_icon('t/grades', '');
+    $currentCourseNode = $parentnode->add($gugcat, $url, navigation_node::NODETYPE_LEAF, $gugcat, 'gugcat', $icon);
 }
 
 function local_gugcat_extend_navigation($navigation){
--- local/gugcat/tests/lib_test.php
+++ local/gugcat/tests/lib_test.php
@@ -28,26 +28,45 @@ defined('MOODLE_INTERNAL') || die();
 global $CFG;
 require_once($CFG->dirroot.'/local/gugcat/lib.php');
 
-class local_gugcat_lib_testcase extends basic_testcase {
+class local_gugcat_lib_testcase extends advanced_testcase {
+
+    public function setUp() {
+        $this->resetAfterTest();
+
+        $this->user = $this->getDataGenerator()->create_user();
+        $this->course = $this->getDataGenerator()->create_course();
+        $this->coursecontext = context_course::instance($this->course->id);
+        $modulecontext = context_module::instance($assign->cmid);
+        $assign = new assign($modulecontext, false, false);
+        $this->cm = $assign->get_course_module();
+    }
+
     public function test_local_gugcat_extend_navigation_course(){
+        global $COURSE;
+
         $node = new navigation_node(array('text'=>"test"));
         $node2 = new navigation_node(array('text'=>"test"));
-        $url = new moodle_url('/local/gugcat/index.php');
+
+        $url = new moodle_url('/local/gugcat/index.php', array('id' => $COURSE->id));        
         $gugcat = get_string('navname', 'local_gugcat');
-        $icon = new pix_icon('my-media', '', 'local_mymedia');
-        $node2->add($gugcat, $url, navigation_node::TYPE_CONTAINER, $gugcat, 'gugcat', $icon);
-        local_gugcat_extend_navigation_course($node, null, null);
+        $icon = new pix_icon('t/grades', '');
+
+        $node2->get('home');
+        $node2->add($gugcat, $url, navigation_node::NODETYPE_LEAF, $gugcat, 'gugcat', $icon)->showinflatnavigation = true;
+        local_gugcat_extend_navigation($node);
         $this->assertEquals($node2, $node);
     }
 
     public function test_local_gugcat_extend_navigation(){
         $node = new navigation_node(array('text'=>"test"));
         $node2 = new navigation_node(array('text'=>"test"));
-        $url = new moodle_url('/local/gugcat/index.php');
+
+        $url = new moodle_url('/local/gugcat/index.php', array('id' => 3));        
         $gugcat = get_string('navname', 'local_gugcat');
-        $icon = new pix_icon('my-media', '', 'local_mymedia');
-        $node2->add($gugcat, $url, navigation_node::TYPE_CONTAINER, $gugcat, 'gugcat', $icon);
-        local_gugcat_extend_navigation_course($node, null, null);
+        $icon = new pix_icon('t/grades', '');
+
+        $node2->add($gugcat, $url, navigation_node::NODETYPE_LEAF, $gugcat, 'gugcat', $icon);
+        local_gugcat_extend_navigation_course($node, (object) array('id'=> 3 ), null);
         $this->assertEquals($node2, $node);
     }
 }
