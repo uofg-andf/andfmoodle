0f9ad8d7cc0e525ecadd1a59a6e1888e15c87d78
elijah.natividad <elijah.natividad@orbium.com>
2021-02-09 16:07:46 +0800

    fix for logs

--- local/gugcat/index.php
+++ local/gugcat/index.php
@@ -129,20 +129,22 @@ $showhidegrade = optional_param('showhidegrade', null, PARAM_NOTAGS);
 $rowstudentid = optional_param('studentid', null, PARAM_NOTAGS);
 $newgrades = optional_param_array('newgrades', null, PARAM_NOTAGS);
 
+//params for logs
+$params = array(
+    'context' => \context_module::instance($selectedmodule->id),
+    'other' => array(
+        'courseid' => $courseid,
+        'activityid' => $activityid,
+        'categoryid' => $categoryid,
+        'page' => $page
+    )
+);
+
 // Process release provisional grades
 if (isset($release)){
     grade_capture::release_prv_grade($courseid, $selectedmodule);
     local_gugcat::notify_success('successrelease');
     //log of release grades
-    $params = array(
-        'context' => \context_module::instance($selectedmodule->id),
-        'other' => array(
-            'courseid' => $courseid,
-            'activityid' => $activityid,
-            'categoryid' => $categoryid,
-            'page' => $page
-        )
-    );
     $event = \local_gugcat\event\release_prv_grade::create($params);
     $event->trigger();
     unset($release);
@@ -161,15 +163,6 @@ if (isset($release)){
         }
         local_gugcat::notify_success('successaddall');
         //log of add multiple grades
-        $params = array(
-            'context' => \context_module::instance($selectedmodule->id),
-            'other' => array(
-                'courseid' => $courseid,
-                'activityid' => $activityid,
-                'categoryid' => $categoryid,
-                'page' => $page
-            )
-        );
         $event = \local_gugcat\event\add_multiple_grades::create($params);
         $event->trigger();
     }else{
@@ -186,17 +179,6 @@ if (isset($release)){
     if ($valid_22point_scale){
         grade_capture::import_from_gradebook($courseid, $selectedmodule, $activities);
         local_gugcat::notify_success('successimport');
-
-        //logging of import grades
-        $params = array(
-            'context' => \context_module::instance($selectedmodule->id),
-            'other' => array(
-                'courseid' => $courseid,
-                'activityid' => $activityid,
-                'categoryid' => $categoryid,
-                'page' => $page
-            )
-        );
         $event = \local_gugcat\event\import_grade::create($params);
         $event->trigger();
     }else{
